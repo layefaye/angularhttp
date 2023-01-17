2bc9f2ac4973fe06661552b8a5b89c06d18b75d4
layefaye <layefayenevrose@gmail.com>
2023-01-17 14:51:36 +0100

    add bootstrap in the project

--- src/app/component/users/users.component.ts
+++ src/app/component/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/interface/user.interface';
 import { UserService } from 'src/app/service/user.service';
 
 @Component({
@@ -7,6 +8,7 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./users.component.css'],
 })
 export class UsersComponent {
+  users: any[] = [];
   constructor(private userService: UserService) {}
   ngOnInit(): void {
     this.userService.getUsers(15).subscribe((results: any) => {
@@ -16,5 +18,13 @@ export class UsersComponent {
     this.userService.getUser(12).subscribe((result: any) => {
       console.log(result);
     });
+    // this.getUsers(12);
   }
+
+  // getUsers(size: number): void {
+  //   this.userService.getUsers(15).subscribe((results: any) => {
+  //     console.log(results);
+  //     this.users = <User[]>results;
+  //   });
+  // }
 }
